29163fe26e8c9997290d8ce455767f339d5058e9
Mathias Bynens <mathias@qiwi.be>
2016-03-21 17:07:36 +0100

    Update icons
    Icon `0x07` is a spear instead of a shovel, as per http://tibia.wikia.com/wiki/Talk:Automap#Map_icons.

--- src/icons.js
+++ src/icons.js
@@ -1,24 +1,24 @@
 const byID = {
-	'0': 'checkmark', // green checkmark ✔
-	'1': '?', // blue question mark ❓
-	'2': '!', // red exclamation mark ❗
-	'3': 'star', // orange star 🟊
-	'4': 'crossmark', // bright red crossmark ❌
-	'5': 'cross', // dark red cross 🕇
-	'6': 'mouth', // mouth with red lips 👄
-	'7': 'shovel', // shovel ⛏
-	'8': 'sword', // sword ⚔
-	'9': 'flag', // blue flag ⚑
-	'10': 'lock', // golden lock 🔒
-	'11': 'bag', // brown bag 👛
-	'12': 'skull', // skull 💀
-	'13': '$', // green dollar sign 💰💲
-	'14': 'red up', // red arrow up ⬆️🔺
-	'15': 'red down', // red arrow down ⬇🔻
-	'16': 'red right', // red arrow right ➡️
-	'17': 'red left', // red arrow left ⬅️
-	'18': 'up', // green arrow up ⬆
-	'19': 'down' // green arrow down ⬇
+	0x00: 'checkmark', // green checkmark ✔
+	0x01: '?', // blue question mark ❓
+	0x02: '!', // red exclamation mark ❗
+	0x03: 'star', // orange star 🟊
+	0x04: 'crossmark', // bright red crossmark ❌
+	0x05: 'cross', // dark red cross 🕇
+	0x06: 'mouth', // mouth with red lips 👄
+	0x07: 'spear', // spear 🏹
+	0x08: 'sword', // sword ⚔
+	0x09: 'flag', // blue flag ⚑
+	0x0A: 'lock', // golden lock 🔒
+	0x0B: 'bag', // brown bag 👛
+	0x0C: 'skull', // skull 💀
+	0x0D: '$', // green dollar sign 💰💲
+	0x0E: 'red up', // red arrow up ⬆️🔺
+	0x0F: 'red down', // red arrow down ⬇🔻
+	0x10: 'red right', // red arrow right ➡️
+	0x11: 'red left', // red arrow left ⬅️
+	0x12: 'up', // green arrow up ⬆
+	0x13: 'down' // green arrow down ⬇
 };
 
 const byName = {};
