9458a752d21ca836ae085b2ca76367b0023ce06c
Raphael Mobis Tacla <r.mobis@gmail.com>
2022-11-01 22:40:50 -0300

    rewrite tests script in javascript for improved interoperability

---
+++ test/test.mjs
@@ -0,0 +1,34 @@
+import { execSync } from 'node:child_process';
+import { dirname } from 'node:path';
+import { chdir } from 'node:process';
+import { fileURLToPath } from 'node:url';
+import { compareDir, readFile } from './util.mjs';
+
+chdir(dirname(fileURLToPath(import.meta.url)));
+
+execSync('npm link');
+execSync('tibia-maps --from-minimap=minimap --output-dir=data');
+
+
+// Check if the generated map files based on the generated PNG and JSON data
+// match the original map files, and call out any differences.
+execSync('tibia-maps --from-data=data --output-dir=minimap-new');
+compareDir('minimap');
+
+
+// Check if `--overlay-grid` works correctly.
+execSync('tibia-maps --from-data=data --output-dir=minimap-grid-new --overlay-grid');
+compareDir('minimap-grid');
+
+
+// Check if `--extra` works correctly.
+execSync('tibia-maps --from-data=data --output-dir=minimap-extra-new --extra=achievements');
+compareDir('minimap-extra');
+
+
+// Check if `--no-markers` skips importing the marker data.
+execSync('tibia-maps --from-minimap=minimap --output-dir=data-without-markers --no-markers');
+const markers = JSON.parse(readFile('data-without-markers/markers.json'));
+if (markers.length > 0) {
+	console.error('Error: `--no-markers` extracted marker data anyway! (data-without-markers/markers.json)');
+}
